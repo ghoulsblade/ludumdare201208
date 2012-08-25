f59f3a5513ef2e10803839f8d6b4c30115f0dabe
ghoulsblade <ghoulsblade@schattenkind.net>
2012-08-25 19:28:20 +0200

    basics + quick title screen hack in gimp

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ludumdare201208
 ===============
 
-http://www.ludumdare.com/ 2012-08 entry with Löve2D
+http://www.ludumdare.com/ 2012-08 entry with Love2D
+
+Love2D ludumdare #24 thread https://love2d.org/forums/viewtopic.php?f=5&t=10645
+
+
---
+++ main.lua
@@ -0,0 +1,21 @@
+
+function GfxSetPixelArtFilter (gfx) gfx:setFilter("nearest","nearest") return gfx end
+
+function love.load()
+	gTitleScreen = GfxSetPixelArtFilter(love.graphics.newImage("data/titelscreen.jpg"))
+end
+
+function love.mousepressed(x,y,btn)
+end
+
+function love.keypressed( key, unicode )
+    gKeyPressed[key] = true
+    if (key == "escape") then os.exit(0) end
+end
+
+function love.update( dt )
+end
+
+function love.draw()
+	love.graphics.draw(gTitleScreen, 0,0, 0, 1,1)
+end
