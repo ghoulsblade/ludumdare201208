72111e5d77edf4c5bf926515494cfefbe9145e83
ghoulsblade <ghoulsblade@schattenkind.net>
2012-08-26 14:37:54 +0200

    healthbar-icons and death

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 love.filesystem.load("lib.oop.lua")()
 
 kTileSize = 64
+kIconSize = 16
 SPEED_PLAYER = 200 -- pixels per second
 SPEED_ENEMY = SPEED_PLAYER*0.5 -- pixels per second
 STOPDIST_PLAYER_MOUSE = 20 -- pixels
@@ -16,6 +17,7 @@ VEL_DAMP = 0.95
 PLAYER_ATTACK_INTERVAL = 0.2 -- seconds
 MOBILE_ATTACK_INTERVAL = 0.2 -- seconds
 MOBILE_ATTACK_ANIM_DUR = 0.1 -- seconds
+PLAYER_START_DEF = 5
 
 gKeyPressed = {}
 gTitleScreen = true
@@ -55,8 +57,12 @@ function love.load()
 	img_part_sword2		= myimg("data/part-sword2.png"		)
 	img_part_sword		= myimg("data/part-sword.png"		)
 	
-	
+	img_icon_shield_half= myimg("data/icon-shield-half.png"	)
+	img_icon_shield		= myimg("data/icon-shield.png"		)
+	img_icon_sword		= myimg("data/icon-sword.png"		)
+
 	img_shadow			= myimg("data/shadow.png"			)
+	
 	img_tile_cave		= myimg("data/tile-cave.png"		)
 	img_tile_cave_floor	= myimg("data/tile-cave-floor.png"	)
 	img_tile_cave_wall	= myimg("data/tile-cave-wall.png"	)
@@ -67,6 +73,7 @@ function love.load()
 	img_tile_sand		= myimg("data/tile-sand.png"		)
 	img_tile_water		= myimg("data/tile-water.png"		)
 	img_tile_sand_water	= myimg("data/tile-sand-water.png"	)
+	
 	img_screen_title	= myimg("data/screen-title.jpg"		)
 end
 
@@ -75,9 +82,9 @@ function StartGame ()
 	
 	local e = kTileSize
 	local ox,oy = kTileSize/2, kTileSize/2
-	local tx,ty=4,4 cMobEnemy:New(img_mob_att, e*tx+ox,e*ty+oy)
-	local tx,ty=6,4 cMobEnemy:New(img_mob_def, e*tx+ox,e*ty+oy)
-	local tx,ty=7,1 cMobEnemy:New(img_mob_def, e*tx+ox,e*ty+oy)
+	local tx,ty=4,4 cMobEnemy:New(img_mob_att, e*tx+ox,e*ty+oy, 2,1)
+	local tx,ty=6,4 cMobEnemy:New(img_mob_def, e*tx+ox,e*ty+oy, 1,2)
+	local tx,ty=7,1 cMobEnemy:New(img_mob_def, e*tx+ox,e*ty+oy, 1,2)
 	local tx,ty=4,6 gPlayer = cMobPlayer:New(img_mob_player, e*tx+ox,e*ty+oy)
 	
 end
@@ -222,8 +229,10 @@ end
 -- ***** ***** ***** ***** ***** cMobBase
 cMobBase = CreateClass()
 
-function cMobBase:Init (img,x,y) 
+function cMobBase:Init (img,x,y,att,def) 
 	self.img = img
+	self.att = att or 1
+	self.def = def or 1
 	self.x = x
 	self.y = y
 	self.vx = 0
@@ -287,24 +296,38 @@ end
 function cMobBase:VectorToMob (mob) return mob.x-self.x,mob.y-self.y end
 
 function cMobBase:Attack (mob) 
+	if (self.dead) then return end
 	if (self.next_attack > gCurTime) then return end
 	
 	self.next_attack = gCurTime + self.attack_interval
 	self.attack_anim_until = gCurTime + self.attack_anim_dur
 	if (mob) then 
 		mob:AddHitVel(self:VectorToMob(mob))
+		mob:TakeDamage(self.att,self)
 	end
 	-- TODO : sound
 end
 
+function cMobBase:TakeDamage (dmg,attacker) 
+	self.def = self.def - dmg
+	if (self.def <= 0) then
+		self:Die(self)
+	end
+end
+
+function cMobBase:Die (attacker) 
+	self.dead = true -- die
+end
 
 function cMobBase:AutoAttack () -- swing wildly and always, even if not in range, used by player when holding down key
+	if (self.dead) then return end
 	self:Attack(nil)
 end
 
 function cMobBase:Draw () 
+	if (self.dead) then return end
 	local ox,oy = -kTileSize/2, -kTileSize/2
-	local x,y = self.x+ox,self.y+oy
+	local x,y = floor(self.x+ox),floor(self.y+oy)
 	love.graphics.draw(img_shadow,x,y)
 	local t = gCurTime + self.anim_random_addt -- seconds
 	local fdur = 0.1 -- frame duration, seconds
@@ -329,6 +352,19 @@ function cMobBase:Draw ()
 	else
 		love.graphics.draw(img_part_sword,x+sword_ox,breathe_y)
 	end
+	
+	-- healtbar
+	local ix,iy = x,y-kIconSize
+	for i=1,self.att do 
+		love.graphics.draw(img_icon_sword,ix,iy)
+		ix = ix + kIconSize/2
+	end
+	for i=1,self.def do
+		if (math.fmod(i-1,2) == 0) then
+			love.graphics.draw((self.def > i) and img_icon_shield or img_icon_shield_half,ix,iy)
+			ix = ix + kIconSize
+		end
+	end	
 end
 
 -- ***** ***** ***** ***** ***** cMobEnemy
@@ -336,13 +372,16 @@ cMobEnemy = CreateClass(cMobBase)
 function cMobEnemy:Init (...) cMobBase.Init(self,...) self.is_enemy = true end
 
 function cMobEnemy:Step (dt)
-	local bWalkToPlayer = true 
 	self:VelStep(dt)
+	if (self.dead) then return end
+	self.walking = false
+	if (gPlayer.dead) then return end
+	local bWalkToPlayer = true 
 	
 	local other,d = GetNearestEnemyToPos(self.x,self.y,self)
-	if (d < ENEMY_SPREAD_DIST) then self:WalkAwayFromMob(other,SPEED_ENEMY,9999,dt) bWalkToPlayer = false end
+	if (d < ENEMY_SPREAD_DIST) then self:WalkAwayFromMob(other,SPEED_ENEMY,9999,dt) self.walking = true bWalkToPlayer = false end
 	
-	if (bWalkToPlayer) then self:WalkToMob(gPlayer,SPEED_ENEMY,STOPDIST_ENEMY_TARGET,dt) end
+	if (bWalkToPlayer) then self:WalkToMob(gPlayer,SPEED_ENEMY,STOPDIST_ENEMY_TARGET,dt) self.walking = true end
 	
 	if (self:DistToMob(gPlayer) < ENEMY_ATTACK_RANGE) then self:Attack(gPlayer) end
 end
@@ -352,9 +391,11 @@ cMobPlayer = CreateClass(cMobBase)
 function cMobPlayer:Init (...) 
 	cMobBase.Init(self,...) 
 	self.attack_interval = PLAYER_ATTACK_INTERVAL
+	self.def = PLAYER_START_DEF
 end
 
 function cMobPlayer:AutoAttack () -- swing wildly and always, even if not in range, used by player when holding down key
+	if (self.dead) then return end
 	local mob,d = GetNearestEnemyToPos(self.x,self.y,self)
 	if (d > PLAYER_ATTACK_RANGE) then mob = nil end
 	self:Attack(mob)
