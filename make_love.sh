0367f3cb8e7896f5877992658ca97ae9338e7624
ghoulsblade <ghoulsblade@schattenkind.net>
2012-08-27 02:31:29 +0200

    make script and titlescreen

---
+++ make_love.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILENAMEBASE=GeneRogue-v1.0
+
+FINDCMD="find . -false" # warning: quoted paths are escaped in a weird way by bash, breaking the find commandline
+FINDCMD="$FINDCMD -o -wholename ./.git -prune"
+FINDCMD="$FINDCMD -o -wholename ./build -prune"
+FINDCMD="$FINDCMD -o -wholename ./screencapture -prune"
+FINDCMD="$FINDCMD -o -wholename ./synctoweb.sh -prune"
+FINDCMD="$FINDCMD -o -print"
+
+rm -f build/${FILENAMEBASE}.love build/${FILENAMEBASE}.exe build/${FILENAMEBASE}-src.zip
+zip /tmp/${FILENAMEBASE}.zip `$FINDCMD`
+cp /tmp/${FILENAMEBASE}.zip build/${FILENAMEBASE}-src.zip
+mv /tmp/${FILENAMEBASE}.zip build/${FILENAMEBASE}.love
+cat build/love/love.exe build/${FILENAMEBASE}.love > build/${FILENAMEBASE}.exe
+
+# make zip for windows with .exe and love dlls + readme
+cd build
+rm -rf ${FILENAMEBASE}/
+mkdir -p ${FILENAMEBASE}/
+mv ${FILENAMEBASE}.exe ${FILENAMEBASE}/
+cp love/* ${FILENAMEBASE}/
+rm ${FILENAMEBASE}/love.exe
+cp ../README.md ${FILENAMEBASE}/
+rm -rf ${FILENAMEBASE}.zip
+zip ${FILENAMEBASE}.zip ${FILENAMEBASE}/*
+rm -rf ${FILENAMEBASE}/
